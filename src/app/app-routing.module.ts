cc236686fbfcd74d2ad4129e5323f8740ec9c1e5
Gianluca Picierro <gianluca.picierro11@ibm.com>
2021-11-10 18:01:05 +0100

    create homepage and add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: "", redirectTo: 'home-page', pathMatch: 'full' },
+      { path: 'en', redirectTo: 'home-page', pathMatch: 'full' },
+      { path: 'home-page', pathMatch: 'full', component: HomepageComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+}
