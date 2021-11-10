c8106cfc87d24d6bd34fde944fd0f2905cdbd75d
Gianluca Picierro <gianluca.picierro11@ibm.com>
2021-11-10 17:34:06 +0100

    Update angular 13

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'erasmus-world-UI'`, () => {
+  it(`should have as title 'erasmus-world-ui'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('erasmus-world-UI');
+    expect(app.title).toEqual('erasmus-world-ui');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('erasmus-world-UI app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('erasmus-world-ui app is running!');
   });
 });
