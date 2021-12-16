afa20d7de1f3728d86f4aaa2f564d150a08dab3e
Gianluca Picierro <gianluca.picierro11@ibm.com>
2021-12-16 15:25:33 +0100

    fix BASE_URL_API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   API_URL_TEST: 'https://erasmus-world-api.herokuapp.com/test/',
   AUTH_API: 'https://erasmus-world-api.herokuapp.com/auth/',
-  BASE_API: 'https://erasmus-world-api.herokuapp.com/'
+  BASE_URL_API: 'https://erasmus-world-api.herokuapp.com/'
 
 };
