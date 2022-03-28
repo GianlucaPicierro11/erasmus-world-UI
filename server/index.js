c1711871f93ed248f08ef07dc5fa8995abbc0c99
Gianluca Picierro <gianluca.picierro11@ibm.com>
2022-03-28 15:30:52 +0200

    work on index.js copy from guide

---
+++ server/index.js
@@ -0,0 +1,18 @@
+//https://medium.com/@intellix/production-ready-angular-cli-in-heroku-4bcdc8322550
+//https://medium.com/@intellix/production-ready-angular-cli-v1-0-0-with-i18n-and-localised-urls-1f3cf16cc204
+const compression = require('compression');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+// Gzip
+app.use(compression());
+// Serve static files from the dist directory
+app.use(express.static(__dirname + '/../dist'));
+// Start the app by listening on the default Heroku port
+app.listen(port);
+// Return index.html for all GET requests for PathLocationStrategy
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../dist/index.html'));
+});
+console.log(`Server listening on ${port}`);
