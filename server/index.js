f76281d768a24e1898659235c02931551aa46c54
Gianluca Picierro <gianluca.picierro11@ibm.com>
2022-03-28 16:44:20 +0200

    work on index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.listen(port);
 // Return index.html for all GET requests for PathLocationStrategy
 // And accept locale style URLs: /en/example
 app.get('/*', (req, res) => {
-    const matches = req.url.match(/^\/([a-z]{2}?)\//);
+    const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
     console.log(`matches ${matches}`);
     const locale = matches && supportedLocales.indexOf(matches[1]) !== -1 ? matches[1] : req.locale;
     console.log(`locale ${locale}`);
